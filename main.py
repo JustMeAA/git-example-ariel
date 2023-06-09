c9f8ca10d0c233d962f5d8d6642923824460d7e5
Ariel <rylnqry@gmail.com>
2023-06-09 20:02:44 +0300

    firstQuestion for us!

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ this is my project, and here we are gonna do fun somethings!
 
 
 print("\t\t\t\t\t\t\t\t\t\t Hey, and welcome to our trivia!")
+
+answer_user = input("What is capital city of Israel?\n1.Tel Aviv\n2.Beer Sheva\n3.Haifa\n4.Jerusalem\n").lower()
+if answer_user == "jerusalem":
+    print("All the respect! you right!")
+else:
+    print("So close! but the answer is... - jerusalem")
+
+
+
+
